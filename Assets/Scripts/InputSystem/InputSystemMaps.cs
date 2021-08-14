b2b2f064f95e3dd1b99bc1eac57c774be25c1924
YouMuCZ <youmucz@gmail.com>
2021-08-14 08:46:48 +0800

    update: 同步unity版本

--- Assets/Scripts/InputSystem/InputSystemMaps.cs
+++ Assets/Scripts/InputSystem/InputSystemMaps.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/InputSystemMaps.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/InputSystem/InputSystemMaps.inputactions'
 
 using System;
 using System.Collections;
